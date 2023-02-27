72f04175f6e1d51d7365052f8126ffe3fb6c8a21
OrihihosAlt <126429181+OrihihosAlt@users.noreply.github.com>
2023-02-26 18:38:53 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+name = input ("What is your name?")
+print("Hi " + name + "!")
+gender = input ("What is your gender?")
+if gender == "male":
+    print("That's great man!")
+elif gender == "female":
+    print("That's great, ma'am.")
+else:
+    print("Good for you!")
+a = input("Would you like to do some math?")
+if a == "Yes" or "Sure" or "Yeah":
+    print("Alright!")
+else:
+    print("Oh, ok.")
